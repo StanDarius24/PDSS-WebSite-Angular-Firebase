a6927fc01457173194b2901f81172b7e479e4182
DrgOv <ovidiu.dragoi00@gmail.com>
2020-12-29 22:00:59 +0200

    removed guards (FIX NEEDED)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,12 @@ import {AboutComponent} from './components/about/about.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {AdminComponent} from './components/admin/admin.component';
-import {AuthGuard} from './core/services/guards/auth.guard';
-import {AdminGuard} from './core/services/guards/admin.guard';
 import {HomeComponent} from './components/home/home.component';
-import {UserGuard} from './core/services/guards/user.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    canActivate: [AuthGuard]
+    component: HomeComponent
   },
   {
     path: 'welcome',
