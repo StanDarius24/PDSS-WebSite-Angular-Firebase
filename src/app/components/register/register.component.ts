9e62de4d45121dfc0359b55d172221476aec45d9
DrgOv <ovidiu.dragoi00@gmail.com>
2020-12-29 21:48:58 +0200

    added register form validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {Component, forwardRef, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, NG_VALIDATORS, Validators} from '@angular/forms';
 import {AuthService} from '../../core/services/auth.service';
 import {User} from '../../core/models/user.model';
 
@@ -20,10 +20,35 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-      email: '',
-      displayName: '',
-      password: ''
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      displayName: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern('(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$')
+      ]]
     });
+  }
+
+  get emailControl() {
+    return this.registerForm.controls.email;
+  }
+
+  get displayNameControl() {
+    return this.registerForm.controls.displayName;
+  }
+
+  get passwordControl() {
+    return this.registerForm.controls.password;
+  }
+
+  get isFormValid() {
+    return this.registerForm.valid;
   }
 
   async onRegister() {
