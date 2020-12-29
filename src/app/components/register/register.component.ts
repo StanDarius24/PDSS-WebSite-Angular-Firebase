55ded794f53ef6ab49dbdb8b7ef5b4af0eea1352
DrgOv <ovidiu.dragoi00@gmail.com>
2020-12-29 21:56:20 +0200

    added login form validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -47,7 +47,7 @@ export class RegisterComponent implements OnInit {
     return this.registerForm.controls.password;
   }
 
-  get isFormValid() {
+  isFormValid() {
     return this.registerForm.valid;
   }
 
