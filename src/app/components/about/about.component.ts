d21700b520f8178fafafc219df0f09528243d542
DrgOv <ovidiu.dragoi00@gmail.com>
2020-12-29 21:06:10 +0200

    made some code cleanup

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: [
+    '../style/style.scss',
+    './about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: [
+    '../style/style.scss',
+    './products.component.scss']
 })
 export class ProductsComponent implements OnInit {
 
